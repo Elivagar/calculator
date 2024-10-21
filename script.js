442e833c556117ebba351927a3451280084aac19
Helgrindi <v.decoen@gmail.com>
2024-10-21 22:58:16 +0200

    Calculator functioning properly, only missing keyboard input.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operator functions
 function add(a, b) {
     return a + b;
 }
@@ -14,16 +15,161 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(add(2, 5));
-console.log(subtract(5, 2));
-console.log(multiply(4, 5));
-console.log(divide(15, 3));
+// Calculator logic
+let firstNumber = "";
+let secondNumber = "";
+let savedOperator = undefined;
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operator;
+const divideButton = document.querySelector("#divide");
+divideButton.addEventListener("click", () => {
+    if (firstNumber !== "" && displayValue !== "") {
+        secondNumber = displayValue;
+        operate(savedOperator);
+    }
 
-function operate(operator, a, b) {
-    
+    savedOperator = divide;
+    firstNumber = display.innerHTML;
+    displayValue = "";
+    operatorUsed = true;
+});
+
+const multiplyButton = document.querySelector("#multiply");
+multiplyButton.addEventListener("click", () => {
+    if (firstNumber !== "" && displayValue !== "") {
+        secondNumber = displayValue;
+        operate(savedOperator);
+    }
+
+    savedOperator = multiply;
+    firstNumber = display.innerHTML;
+    displayValue = "";
+    operatorUsed = true;
+});
+const subtractButton = document.querySelector("#subtract");
+subtractButton.addEventListener("click", () => {
+    if (firstNumber !== "" && displayValue !== "") {
+        secondNumber = displayValue;
+        operate(savedOperator);
+    }
+
+    savedOperator = subtract;
+    firstNumber = display.innerHTML;
+    displayValue = "";
+    operatorUsed = true;
+});
+
+const addButton = document.querySelector("#add");
+addButton.addEventListener("click", () => {
+    if (firstNumber !== "" && displayValue !== "") {
+        secondNumber = displayValue;
+        operate(savedOperator);
+    }
+
+    savedOperator = add;
+    firstNumber = display.innerHTML;
+    displayValue = "";
+    operatorUsed = true;
+});
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", () => {
+    if (firstNumber === "" && displayValue === "") return;
+
+    if (savedOperator) {
+    secondNumber = displayValue;
+    operate(savedOperator);
+    savedOperator = undefined;
+    displayValue = "";
+    operatorUsed = false;
+    }
+});
+
+function operate(operator) {
+    let a = Number(firstNumber);
+    let b = Number(secondNumber);
+
+    let result;
+    if (operator === add) {
+        result = add(a, b);
+    } else if (operator === subtract) {
+        result = subtract(a, b);
+    } else if (operator === multiply) {
+        result = multiply(a, b);
+    } else if (operator === divide) {
+        result = divide(a, b);
+    }
+
+    if (result === Infinity || result === NaN) {
+        display.innerHTML = "OH HELL NO";
+        displayValue = "";
+        savedOperator = undefined;
+        operatorUsed = false;
+        firstNumber = "";
+        secondNumber = "";
+    } else {
+        result = Math.round(result * 1000) / 1000;
+        display.innerHTML = result;
+        firstNumber = result;
+        secondNumber = "";
+    }
 }
 
+
+
+// Writing and populating display
+let numberButton = document.querySelectorAll(".number");
+let zeroButton = document.querySelector("#num0");
+let commaButton = document.querySelector("#comma");
+let display = document.querySelector(".workingField");
+let operatorUsed = false;
+let displayValue = "";
+display.innerHTML = "0";
+
+numberButton.forEach(button => {
+    button.addEventListener("click", () => {
+        if (display.innerHTML === "0" || display.innerHTML === "OH HELL NO") {
+            display.innerHTML = "";
+        } else if (operatorUsed === true) {
+            operatorUsed = false;
+            display.innerHTML = "";
+        }
+
+        const buttonText = button.textContent;
+        displayValue += buttonText;
+        display.innerHTML += buttonText;
+    });
+});
+
+commaButton.addEventListener("click", () => {
+    if (display.innerHTML === "0" || display.innerHTML === "OH HELL NO") {
+        displayValue = "0.";
+        display.innerHTML = "0.";
+    } else if (!display.innerHTML.includes(".")) {
+        displayValue += ".";
+        display.innerHTML += ".";
+    }
+});
+
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+    displayValue = "";
+    display.innerHTML = "0";
+    savedOperator = undefined;
+    operatorUsed = false;
+    firstNumber = "";
+    secondNumber = "";
+});
+
+let backspaceButton = document.querySelector("#backspace");
+backspaceButton.addEventListener("click", () => {
+    if (displayValue === "") {
+        return;
+    } else {
+        displayValue = displayValue.slice(0, -1);
+        display.innerHTML = displayValue;
+
+        if (display.innerHTML === "") {
+            display.innerHTML = "0";
+        }
+    }
+});
